3071e33905c35e641241a4e445ec27055d5cf1fc
oniszczak <aleks42@gmail.com>
2023-01-24 20:36:23 -0500

    Initial Commit

---
+++ MyWorkouts Watch App/MyWorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWorkoutsApp.swift
+//  MyWorkouts Watch App
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct MyWorkouts_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
